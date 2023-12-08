76bc6d1e0aad223a51360832ef57332c874384bf
Marine-Sanson <marine_sanson@yahoo.fr>
2023-12-08 16:36:49 +0100

    [#7] Changes after Antoine's review

--- src/Service/TrickService.php
+++ src/Service/TrickService.php
@@ -4,18 +4,20 @@ namespace App\Service;
 
 use App\Entity\Trick;
 use App\Mapper\TricksMapper;
+use App\Repository\TrickRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class TrickService
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
+        private readonly TrickRepository $trickRepository,
         private readonly TricksMapper $tricksMapper
         ) { }
 
     public function getHomeTricks(): array
     {
-        $tricks = $this->entityManager->getRepository(Trick::class)->findAll();
+        $tricks = $this->trickRepository->findAll();
         return $this->tricksMapper->transformToHomeTricks($tricks);
     }
 
